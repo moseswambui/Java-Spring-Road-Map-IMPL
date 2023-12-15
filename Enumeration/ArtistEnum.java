1cec933654afba71416fcefff7481c74dc4b9ad2
moses wambui <moses.wambui@smart.local>
2023-12-16 00:40:29 +0300

    Revisit on Enum - Unfinished changes

---
+++ Enumeration/ArtistEnum.java
@@ -0,0 +1,43 @@
+package Enumeration;
+
+
+enum Artists{
+    LEONARDO_DAVINCI,
+    MADONA,
+    MONA_LISA,
+    SALVATOR_MUNDI,
+
+
+}
+
+/*
+
+
+public enum ArtistEnum {
+    public static void main(String[] args) {
+        // Using enum values
+        Artists artist = Artists.LEONARDO_DAVINCI;
+
+        // Switch statement with enums
+        switch (artist){
+            case LEONARDO_DAVINCI:
+                System.out.println(" Leonardo da Vinci, c. 1503–19");
+                break;
+            case MADONA:
+                System.out.println("The Virgin of the Rocks (c. 1483–86)");
+                break;
+
+            case MONA_LISA:
+                System.out.println("Head of a Woman (1500–10)");
+                break;
+
+            case SALVATOR_MUNDI:
+                System.out.println("Lady with an Ermine (c. 1489–91)");
+                break;
+            default:
+                System.out.println("Its Just Another Artist: Moses Ginevra de’ Benci (c. 1474/78) ");
+        }
+    }
+
+}
+ */
--- Enumeration/enumaration.java
+++
@@ -1,9 +0,0 @@
-package Enumeration;
-
-import java.util.Enumeration;
-import java.util.Vector;
-
-public enum enumaration {
-
-
-}
